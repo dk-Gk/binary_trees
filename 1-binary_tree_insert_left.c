b1d3565f94820b8c1986675fe92ed8f07a6cb283
dk-Gk <idkin19@gmail.com>
2022-05-16 15:25:02 +0000

    binary_tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,21 +13,18 @@ if (!parent)
 {
 return (NULL);
 }
-binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+binary_tree_t *new_node = calloc(1, sizeof(binary_tree_t));
 if (new_node == NULL)
 return (NULL);
 
 new_node->n = value;
-new_node->left = NULL;
-new_node->right = NULL;
 new_node->parent = parent;
-if (parent->left == NULL)
-parent->left = new_node;
-else
+
+if (parent->left)
 {
 new_node->left = parent->left;
-parent->left->parent = new_node;
-parent->left = new_node;
+new_node->left-> parent = new_node;
 }
+parent->left = new_node;
 return (new_node);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,27 +14,20 @@ if (!parent)
 {
 return (NULL);
 }
-binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+binary_tree_t *new_node = calloc(1, sizeof(binary_tree_t));
 
 if (!new_node)
 {
 return (NULL);
 }
 new_node->n = value;
-new_node->left = NULL;
-new_node->right = NULL;
 new_node->parent = parent;
 
-if (parent->right == NULL)
-{
-parent->right = new_node;
-}
-
-else
+if (parent->right)
 {
 new_node->right = parent->right;
-parent->right->parent = new_node;
-parent->right = new_node;
+new_node->right->parent = new_node;
 }
+parent->right = new_node;
 return (new_node);
 }
